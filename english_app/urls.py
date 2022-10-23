64e863ddb41326520bbb3258dfc6e8fc1b457c94
sanjarbek <sanjarsaidov173@gmail.com>
2022-10-23 13:46:11 +0500

    views

--- english_app/urls.py
+++ english_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_data, name='getting'),
+    path('', views.home, name='home'),
+    path('get', views.get_data, name='getting'),
+    path('add', views.put_data, name='add')
 ]
--- english_app/views.py
+++ english_app/views.py
@@ -3,6 +3,10 @@ from rest_framework.decorators import api_view
 from .models import Question
 
 @api_view(['GET'])
+def home(request):
+    return Response({'status':'ok'})
+
+@api_view(['GET'])
 def get_data(request):
     raw = request.GET
     data = Question.objects.filter(name=raw.get('name'))
@@ -12,3 +16,10 @@ def get_data(request):
         dct['answer'] = i.answer
 
     return Response({"data":data})
+
+@api_view(['POST'])
+def put_data(request):
+    raw = request.POST
+    q = Question(name=raw.get('name'), question=raw.get('question'), answer=raw.get('answer'))
+    q.save()
+    return Response({'status':'saved'})
